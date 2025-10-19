c85d9f829e640791bfe18ff9b79471bd207c1925
BMwendwaM <mwendwa600@gmail.com>
2025-10-19 13:16:36 +0300

    Correct FOREIGN KEY syntax and data types.

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -2,34 +2,38 @@ CREATE DATABASE IF NOT EXISTS alx_book_store;
 USE alx_book_store;
 
 CREATE TABLE Books (
-    book_id PRIMARY KEY,
+    book_id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(130),
-    author_id FOREIGN KEY,
+    author_id INT,
+    FOREIGN KEY (author_id) REFERENCES Authors (author_id)
     price DOUBLE,
     publication_date DATE
 );
 
 CREATE TABLE Authors (
-    author_id PRIMARY KEY,
+    author_id INT AUTO_INCREMENT PRIMARY KEY,
     author_name VARCHAR(215)
 );
 
 CREATE TABLE Customers (
-    customer_id PRIMARY KEY,
+    customer_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_name VARCHAR(215),
     email VARCHAR(215),
     address TEXT
 );
 
 CREATE TABLE Orders (
-    order_id PRIMARY KEY,
-    customer_id FOREIGN KEY,
+    order_id INT AUTO_INCREMENT PRIMARY KEY,
+    customer_id INT,
+    FOREIGN KEY (customer_id) REFERENCES Customers (customer_id)
     order_date DATE
 );
 
 CREATE TABLE Order_Details (
-    orderdetailid PRIMARY KEY,
-    order_id FOREIGN KEY,
-    book_id FOREIGN KEY,
+    orderdetailid INT AUTO_INCREMENT PRIMARY KEY,
+    order_id INT,
+    FOREIGN KEY (order_id) REFERENCES Orders (order_id)
+    book_id INT,
+    FOREIGN KEY (book_id) REFERENCES Books (book_id)
     quantity DOUBLE
 );
